f7e4ff850b66aea50109e94cb491eb54bdaf7994
Alex Colby <alexanderdcolby@gmail.com>
2023-11-13 22:33:41 +0000

    feat: adds rectangular sensor colliders

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -1,9 +1,11 @@
 mod camera;
+mod colliders;
 mod coords;
 mod events;
 mod kinetic;
 
 pub use camera::CameraPlugin;
+pub use colliders::RectSensor;
 pub use coords::{CoordsPlugin, MouseCoords};
 pub use events::SpawnEvent;
 pub use kinetic::Kinetic;
